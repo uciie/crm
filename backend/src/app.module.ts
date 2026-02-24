7dadf3b9acdab48ed2bb31f404d9adb03d42084d
uciie <pan.luciie@gmail.com>
2026-02-25 00:29:02 +0100

    [Back] : app.module.ts mis à jour

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,26 +4,27 @@ import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler'
 import { ConfigModule }      from '@nestjs/config'
 import { AuthModule }        from './auth/auth.module'
 import { ContactsModule }    from './contacts/contacts.module'
-//import { LeadsModule }       from './leads/leads.module'
+import { LeadsModule }       from './leads/leads.module'
 import { PipelineModule }    from './pipeline/pipeline.module'
 import { TasksModule }       from './tasks/tasks.module'
 import { CommunicationsModule } from './communications/communications.module'
 //import { EmailModule }       from './email/email.module'
-//import { DashboardModule }   from './dashboard/dashboard.module'
-//
+import { DashboardModule }   from './dashboard/dashboard.module'
+import { CompaniesModule }  from './companies/companies.module'
+
 @Module({
   imports: [
     ThrottlerModule.forRoot([{ ttl: 60000, limit: 20 }]),
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,           //← enregistre JwtAuthGuard et RolesGuard globalement
     ContactsModule,
-    //CompaniesModule,
-    //LeadsModule,
+    CompaniesModule,
+    LeadsModule,
     PipelineModule,
     TasksModule,
     CommunicationsModule,
     //EmailModule,
-    //DashboardModule,
+    DashboardModule,
   ],
   providers: [
     {
